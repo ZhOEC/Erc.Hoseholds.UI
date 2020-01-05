51b56d3f35a45bae9ffb4803556aa995287a26e9
Yaroslav Vishnevsky <rick.ua@gmail.com>
2020-01-05 15:30:58 +0200

    add ng zorro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-
 
 const routes: Routes = [
-  //{ path: '', component: HomeComponent }
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
 ];
 
 @NgModule({
