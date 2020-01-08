cccf7b7ad714b1b9af1605d93c4f3585248bb2fd
Max <ztgordey@gmail.com>
2020-01-08 14:22:45 +0200

    Add drawer; Add username in header;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AuthService } from './core/auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt/src/jwthelper.service';
+import { Observable } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  isCollapsed = false;
+  visibilityDrawer = false;
+  placementDrawer = 'left';
+
+  jwtHelper = new JwtHelperService();
+  currentUser: Promise<string>;
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+  );
+
+  ngOnInit() {
+    this.currentUser = this.getUser();
+  }
+
+  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService) {}
+
+  async getUser() {
+    var tokens = JSON.parse(localStorage.tokens)
+    var access_token = tokens.access_token
+    var decodedToken = this.jwtHelper.decodeToken(access_token);
+    
+    return new Promise<string>((resolve) => resolve(decodedToken.username));
+  }
 }
