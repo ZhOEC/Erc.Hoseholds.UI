f414db07d155d154db503d63244e1bbaab093436
Yaroslav <rick@ztoe.com.ua>
2020-10-22 11:34:01 +0300

    fix bo list

--- src/app/modules/accounting-point/accounting-point-form/accounting-point-form.component.ts
+++ src/app/modules/accounting-point/accounting-point-form/accounting-point-form.component.ts
@@ -82,11 +82,9 @@ export class AccountingPointFormComponent implements OnInit {
 
   getBranchOffices() {
     this.branchOfficeService.getBranchOffices().subscribe(offices => {
-      if (offices.length == 1) {
+      this.branchOfficesList = offices;
+      if (this.branchOfficesList.length == 1) {
         this.accountingPointForm.get('branchOfficeId').setValue(this.branchOfficesList[0].id)
-      }
-      else {
-        this.branchOfficesList = offices;
       }
     })
   }
